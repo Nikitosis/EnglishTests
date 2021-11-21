295c8b59d6c77e88888d48f31602c58783e83bf9
rmochulskyi@grayhairsoftware.com <rmochulskyi@grayhairsoftware.com>
2021-11-21 20:52:58 +0200

    fix AssignmentRepository

--- src/main/java/io/english/repository/AssignmentRepository.java
+++ src/main/java/io/english/repository/AssignmentRepository.java
@@ -1,11 +1,11 @@
 package io.english.repository;
 
 import io.english.entity.dao.Assignment;
-import io.english.repository.specification.AssignmentSearchSpecification;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-    List<Assignment> findAll(AssignmentSearchSpecification specification);
+    List<Assignment> findAll(Specification<Assignment> specification);
 }
